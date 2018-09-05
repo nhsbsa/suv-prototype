a27e1995c87a5c900742056b9d1c757e5a2c0c44
Tim Gorringe <tgorringe4@gmail.com>
2018-09-05 13:46:15 +0100

    work on edit trans and refunds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -65,3 +65,21 @@ $(document).ready(function(){
     
   
 })
+
+$(document).ready(function(){
+
+  //hides dropdown content
+  $(".size_chart").hide();
+  
+  //unhides first option content
+  $("#option1").show();
+  
+  //listen to dropdown for change
+  $("#size_select").change(function(){
+    //rehide content on change
+    $('.size_chart').hide();
+    //unhides current item
+    $('#'+$(this).val()).show();
+  });
+  
+});
